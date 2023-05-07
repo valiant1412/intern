12c6e0e27a13adb1b792b19a0f5a955c4b556837
nguyenminhduc2c19 <nguyenminhduc2c19@gmail.com>
2023-05-07 20:55:15 +0700

    first-commit

---
+++ Account.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Collections;
+using System.Xml.Linq;
+
+namespace ConsoleApp1.OOP.BankAccount
+{
+    class Account
+    {
+        private string name;
+        private string account_number;
+        private double balance;
+
+        public Account(string name, string account_number, double balance)
+        {
+            this.name = name;
+            this.account_number = account_number;
+            this.balance = balance;
+        }
+        public Account() { }
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public string Account_number
+        {
+            get { return this.account_number;}
+            set { this.account_number = value;}
+        }
+        public double Balance
+        {
+            get { return this.balance; }
+            set { this.balance = value; }
+        }
+        public bool CreateAccount()
+        {
+            Console.WriteLine("Type your name");
+            name = Console.ReadLine();
+            string num;
+            bool isTrue =true;
+            string pathname = "C:\\Users\\ASUS\\Desktop\\Learn C#\\ConsoleApp1\\ConsoleApp1\\OOP\\BankAccount\\Account.txt";
+            //validate the account number is exists or not
+            while (true)
+            {
+                Console.WriteLine("Type your account number");
+                account_number = Console.ReadLine();
+                using (StreamReader reader = new StreamReader(pathname))
+                {
+                    string line = reader.ReadLine();
+                    if (line != null)
+                    {
+                        int count = 0;
+                        string[] parts = line.Split(',');
+                        string savedName = parts[0];
+                        string savedNum = parts[1];
+                        if (savedNum == account_number)
+                        {
+
+                            count++;
+                            Console.WriteLine("Your account number is existed");
+                            continue;
+                        }
+                        if (count <1)
+                        {
+                            return true;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+            }
+            
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApp1.OOP.ManageEmployee;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace ConsoleApp1.OOP.BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathname = "C:\\Users\\ASUS\\Desktop\\Learn C#\\ConsoleApp1\\ConsoleApp1\\OOP\\BankAccount\\Account.txt";
+            Account account = new Account();
+            bool create_account = account.CreateAccount();
+            if (create_account)
+            {
+                using (StreamWriter writer = File.AppendText(pathname))
+                {
+                    Console.WriteLine("You have added a new account");
+                    writer.WriteLine(account.Name + "," + account.Account_number);
+                }
+            }
+        }
+    }
+}
